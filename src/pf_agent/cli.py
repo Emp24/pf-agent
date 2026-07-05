d8415b1f159c225d2b5078a41541125353505b85
Mahmoud Khaled <40623900+Emp24@users.noreply.github.com>
2026-07-06 02:00:10 +0300

    feat: added skeleton for typer

---
+++ src/pf_agent/cli.py
@@ -0,0 +1,32 @@
+
+from pathlib import Path
+from typing import Annotated
+
+import typer
+
+app = typer.Typer(help="A CLI APP to review code using an AI agent")
+
+
+@app.command()
+def review(path: Annotated[Path, typer.Argument(exists=True)]):
+    """
+    Command to review your code.
+    Prints the path for now 
+    """
+    typer.echo(f"Would review '{path}'")
+                
+
+
+@app.callback()
+def callback():
+    """
+    Review your code.
+    """
+
+
+def main() -> None:
+    app()
+
+
+if __name__ == "__main__":
+    main()
